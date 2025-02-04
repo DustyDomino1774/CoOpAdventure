3cd2023b71a5b319aac62dc1c0299f06857c31cf
Dominic London <londonrdominic@gmail.com>
2025-02-04 00:54:58 -0800

    Movable Actor Constructor
    Initialized key actor member variable in constructor using the arrow points to for movement.

--- Source/CoOpAdventure/MoveableActor.cpp
+++ Source/CoOpAdventure/MoveableActor.cpp
@@ -9,13 +9,37 @@ AMoveableActor::AMoveableActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	Point1 = CreateDefaultSubobject<UArrowComponent>(TEXT("Point1"));
+	Point1->SetupAttachment(RootComp);
+	Point1->SetRelativeLocation(FVector::Zero());
+
+	Point2 = CreateDefaultSubobject<UArrowComponent>(TEXT("Point2"));
+	Point2->SetupAttachment(RootComp);
+	Point2->SetRelativeLocation(FVector(0.0f, 0.0f, 300.0f));
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComp);
+	Mesh->SetIsReplicated(true);
+
+	Transporter = CreateDefaultSubobject<UTransporter>(TEXT("Transporter"));
+
 }
 
 // Called when the game starts or when spawned
 void AMoveableActor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	FVector StartPoint = GetActorLocation() + Point1->GetRelativeLocation();
+	FVector EndPoint = GetActorLocation() + Point2->GetRelativeLocation();
 	
+	Transporter->SetPoints(StartPoint, EndPoint);
 }
 
 // Called every frame
