2a965f2878e4947a9aef68f689881c05f4866dcd
Dominic London <londonrdominic@gmail.com>
2025-02-18 00:37:38 -0800

    Win RPC
    Created a win condition RPC implementation and a delegate to be called by blueprint binding.

--- Source/CoOpAdventure/WinArea.h
+++ Source/CoOpAdventure/WinArea.h
@@ -7,6 +7,8 @@
 #include "Components/BoxComponent.h"
 #include "WinArea.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWinAreaOnWinCondition);
+
 UCLASS()
 class COOPADVENTURE_API AWinArea : public AActor
 {
@@ -29,4 +31,10 @@ public:
 	
 	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
 	UBoxComponent* WinAreaBox;
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastRPCWin();
+
+	UPROPERTY(BlueprintAssignable)
+	FWinAreaOnWinCondition OnWinCondition;
 };
